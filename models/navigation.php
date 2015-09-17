5e914c3b9a72bbe55a3bf6b62dbfd935f02e0906
Andre Beging <andre.beging@uni-siegen.de>
2015-09-17 18:41:03 +0200

    Restructured and cleaned up navigation

--- models/navigation.php
+++ models/navigation.php
@@ -6,38 +6,33 @@ global $nav;
 $separator = new NavSeparator ();
 
 // USERS
-$dropDown = new NavBarDropDown ( "Users" );
-$dropDown->addItem ( new NavBarItem ( "My Profile", PROJECT_ROOT . "/me", "" ) );
-if (isAdmin () || GODMODE) {
-	$dropDown->addItem ( $separator );
-	$dropDown->addItem ( new NavHeader ( "Admin" ) );
-	$dropDown->addItem ( new NavBarItem ( "All User", PROJECT_ROOT . "/user/overview", "" ) );
-	$dropDown->addItem ( new NavBarItem ( "New User", PROJECT_ROOT . "/user/newuser", "" ) );
-}
-$nav->addItem ( $dropDown );
+$nav->addItem ( new NavBarItem ( "My Profile", PROJECT_ROOT . "/me", "" ) );
+
 
 // GROUPS
-$dropDown = new NavBarDropDown ( "Groups" );
-$dropDown->addItem ( new NavBarItem ( "My Groups", PROJECT_ROOT . "/group/my", "" ) );
-if (isAdmin () || GODMODE) {
-	$dropDown->addItem ( $separator );
-	$dropDown->addItem ( new NavHeader ( "Admin" ) );
-	$dropDown->addItem ( new NavBarItem ( "All Groups", PROJECT_ROOT . "/group/overview", "" ) );
-	$dropDown->addItem ( new NavBarItem ( "New Group", PROJECT_ROOT . "/group/create", "" ) );
-}
-$nav->addItem ( $dropDown );
+#$nav->addItem ( new NavBarItem ( "Groups", PROJECT_ROOT . "/group/my", "" ) );
 
 // PROJECTS
-$dropDown = new NavBarDropDown ( "Projects" );
-$dropDown->addItem ( new NavBarItem ( "My Projects", PROJECT_ROOT . "/project/my", "" ) );
+$nav->addItem ( new NavBarItem ( "Projects", PROJECT_ROOT . "/project/my", "" ) );
+
+// ADMIN
 if (isAdmin () || GODMODE) {
+	$dropDown = new NavBarDropDown ( "Admin" );
+	$dropDown->addItem ( new NavHeader ( "User" ) );
+	$dropDown->addItem ( new NavBarItem ( "All User", PROJECT_ROOT . "/user/overview", "" ) );
+	$dropDown->addItem ( new NavBarItem ( "New User", PROJECT_ROOT . "/user/newuser", "" ) );
 	$dropDown->addItem ( $separator );
-	$dropDown->addItem ( new NavHeader ( "Admin" ) );
+	$dropDown->addItem ( new NavHeader ( "Groups" ) );
+	$dropDown->addItem ( new NavBarItem ( "All Groups", PROJECT_ROOT . "/group/overview", "" ) );
+	$dropDown->addItem ( new NavBarItem ( "New Group", PROJECT_ROOT . "/group/create", "" ) );
+	$dropDown->addItem ( $separator );
+	$dropDown->addItem ( new NavHeader ( "Projects" ) );
 	$dropDown->addItem ( new NavBarItem ( "All Projects", PROJECT_ROOT . "/project/overview", "" ) );
 	$dropDown->addItem ( new NavBarItem ( "New Project", PROJECT_ROOT . "/project/create", "" ) );
+	$nav->addItem ( $dropDown );
 }
-$nav->addItem ( $dropDown );
 
+// LOGOUT
 $nav->addItem ( new NavBarItem ( "Logout", PROJECT_ROOT . "/login/logout", "" ) );
 
 function isAdmin() {
--- views/user/profile.php
+++ views/user/profile.php
@@ -12,15 +12,15 @@ $projects = getProjects ();
 	<div
 		style="margin-left: auto; margin-right: auto; width: 100%; max-width: 400px;">
 		<h3 style="text-align: center;">You are member of <?php echo count($groups);?> Group(s)</h3>
-		<table border="1" style="width: 100%;" class="rowlink table-striped">
-			<tr href="<?php echo PROJECT_ROOT."/group/my"; ?>">
+		<table border="1" style="width: 100%;" class="table-striped">
+			<tr>
 				<th style="padding: 3px;">Name</th>
 				<th style="padding: 3px;">Description</th>
 			</tr>
 	        <?php
 									foreach ( $groups as $g ) :
 										?>
-	        <tr href="<?php echo PROJECT_ROOT."/group/my"; ?>">
+	        <tr>
 				<td style="padding: 3px;"><?php echo $g["name"]; ?></td>
 				<td style="padding: 3px;"><?php echo $g["description"]; ?></td>
 			</tr>
